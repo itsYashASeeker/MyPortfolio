7edc45d8a394a366989cb93b2c35c9781175afff
itsYashASeeker <yashkc6432@gmail.com>
2023-03-19 00:32:28 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,10 @@
+const data = [
+    {
+        id: 1,
+        title: "A React website for buying web3 mobile number",
+        link: "https://itsyashaseeker.github.io/Ultimate-digits/",
+        video: "/Ultimate-digits(sr).mp4",
+    }
+];
+
+export default data;
